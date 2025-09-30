bbd29c9fcc3468c21128d921bd96920a5c24b5eb
HuYang <30764038+Ccccraz@users.noreply.github.com>
2025-09-30 14:01:59 +0200

    feat: :sparkles: add main entry point module

---
+++ src/mxbi/__init__.py
@@ -0,0 +1,12 @@
+from mxbi.theater import Theater
+from mxbi.ui.launch_panel import LaunchPanel
+
+
+def main() -> None:
+    LaunchPanel()
+
+    Theater()
+
+
+if __name__ == "__main__":
+    main()
